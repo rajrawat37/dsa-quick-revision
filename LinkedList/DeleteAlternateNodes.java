953a80713bf34259103985daea646f568e8057e7
Raj Rawat <rawatraj995@gmail.com>
2020-09-30 09:48:48 +0530

    Update DeleteAlternateNodes.java

--- LinkedList/DeleteAlternateNodes.java
+++ LinkedList/DeleteAlternateNodes.java
@@ -1,3 +1,19 @@
+ public void deleteAlternate (Node head){
+         Node prev=head;
+         while(prev!=null && prev.next!=null){
+              prev.next=prev.next.next;
+              prev=prev.next;
+         }
+          
+          while(head!=null){
+          System.out.print(head.data+" ");
+          head=head.next;
+          }
+ }
+
+// Or other method
+
+
 public void deleteAlternate (Node head){
          Node prev=head;
          Node now=head.next;
@@ -10,13 +26,9 @@ public void deleteAlternate (Node head){
                     now = prev.next;
                 }
            }
-          
-          
+                  
           while(head!=null){
           System.out.print(head.data+" ");
           head=head.next;
           }
-            
-      
-    }
 }
