fdd3f0a1258e94413891fc5dd864362941f023bc
Raj Rawat <rawatraj995@gmail.com>
2020-09-30 09:28:42 +0530

    Create DeleteAlternateNodes.java

---
+++ LinkedList/DeleteAlternateNodes.java
@@ -0,0 +1,22 @@
+public void deleteAlternate (Node head){
+         Node prev=head;
+         Node now=head.next;
+          
+           while(prev!=null && now!=null){
+                prev.next=now.next;
+                now=null;
+                prev=prev.next;
+                if(prev!=null){
+                    now = prev.next;
+                }
+           }
+          
+          
+          while(head!=null){
+          System.out.print(head.data+" ");
+          head=head.next;
+          }
+            
+      
+    }
+}
