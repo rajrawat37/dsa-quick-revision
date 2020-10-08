2e4ef3cca1180f016fd690e228cea728611aced8
Raj Rawat <rawatraj995@gmail.com>
2020-10-08 14:18:42 +0530

    Create Remove_Every_Kth_Node.java

---
+++ LinkedList/Remove_Every_Kth_Node.java
@@ -0,0 +1,17 @@
+ Node delete(Node head, int k)
+    {
+           if(k==1)
+             return null;
+           else{
+                int i=1;
+                Node temp=head;
+                while(temp!=null && temp.next!=null){
+                    if(i%(k-1)==0){
+                         temp.next=temp.next.next;
+                    }
+                    temp=temp.next;
+                    i++;
+                }
+           }
+           return head;
+    }
