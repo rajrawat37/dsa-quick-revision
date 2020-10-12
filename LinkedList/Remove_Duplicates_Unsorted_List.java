7da1836070c66d5aead410d4a95dce103cbee67e
Raj Rawat <rawatraj995@gmail.com>
2020-10-12 09:31:31 +0530

    Update Remove_Duplicates_Unsorted_List.java

--- LinkedList/Remove_Duplicates_Unsorted_List.java
+++ LinkedList/Remove_Duplicates_Unsorted_List.java
@@ -14,3 +14,24 @@
         }
         return res.next;
     }
+
+
+//Another Approach
+
+ public Node removeDuplicates(Node head)
+    {
+        Node first = head;
+        Node second = head.next;
+        Set<Integer> set = new HashSet<>();
+        set.add(head.data);
+        while(second!=null) {
+            if(set.contains(second.data)) {
+                first.next = second.next;
+            } else {
+                set.add(second.data);
+                first=first.next;
+            }
+            second = second.next;
+        }
+        return head;
+    }
