7efaa5afa70c3af233eba7c83aefaec657a2f119
Raj Rawat <rawatraj995@gmail.com>
2020-10-14 23:49:01 +0530

    Create Check_Circular_Linked_List.java

---
+++ LinkedList/Check_Circular_Linked_List.java
@@ -0,0 +1,12 @@
+static boolean isCircular( Node head)
+{
+    if (head == null)
+    return true;
+ 
+    Node node = head.next;
+ 
+    while (node != null && node != head)
+        node = node.next;
+    
+    return (node == head);
+}
