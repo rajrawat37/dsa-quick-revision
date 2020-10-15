20a98e8d0f4870934cf76a2b0090fc9ebef33f20
Raj Rawat <rawatraj995@gmail.com>
2020-10-15 16:32:50 +0530

    Update Loop_In_A_Linked_List.java

--- LinkedList/Loop_In_A_Linked_List.java
+++ LinkedList/Loop_In_A_Linked_List.java
@@ -1,20 +1,11 @@
-public static bool detectLoop(Node h) 
-    { 
-        HashSet<Node> s = new HashSet<Node>(); 
+//Approach-1
+public static boolean detectLoop(Node h){
+      HashSet<Node> s = new HashSet<Node>(); 
         while (h != null) { 
-            // If we have already has this node 
-            // in hashmap it means their is a cycle 
-            // (Because you we encountering the 
-            // node second time). 
-            if (s.Contains(h)) 
+            if (s.contains(h)) 
                 return true; 
-  
-            // If we are seeing the node for 
-            // the first time, insert it in hash 
-            s.Add(h); 
-  
+            s.add(h); 
             h = h.next; 
         } 
-  
         return false; 
-    } 
+}
