b25bfa1007ead16011923ffbbc1e64c5ae3254f4
Raj Rawat <rawatraj995@gmail.com>
2020-10-17 22:14:57 +0530

    Create Intersection_Point_In_Y_Shapes_Linked_List.java

---
+++ LinkedList/Intersection_Point_In_Y_Shapes_Linked_List.java
@@ -0,0 +1,34 @@
+
+	int intersectPoint(Node headA, Node headB)
+	{
+  int num_1=getCount(headA);
+	int num_2=getCount(headB);
+	if(num_1>num_2)
+       return	getIntersection (num_1-num_2,headA,headB);
+
+         else 
+          return getIntersection(num_2-num_1,headB,headA);
+	}
+	int getIntersection(int num1,Node head1,Node head2){
+	    for(int i=0;i<num1;i++)
+	    if(head1!=null)
+	    head1=head1.next;
+	    
+	    while(head1!=null&&head2!=null){
+	    if(head1.next==head2.next)
+	    return head1.next.data;
+	    head1=head1.next;
+	    head2=head2.next;
+	    }
+	    return -1;
+	    
+	}
+	int getCount(Node head){
+	    int num=0;
+	    while(head!=null)
+	    {
+	        num++;
+	        head=head.next;
+	    }
+	    return num;
+	}
