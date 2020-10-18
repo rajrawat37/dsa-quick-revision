230d2787e904ff5695ac1c95dd6ca26ef2f0e4ee
Raj Rawat <rawatraj995@gmail.com>
2020-10-18 23:42:28 +0530

    Create Swap_Nodes_Without_Swapping_Data.java

---
+++ LinkedList/Swap_Nodes_Without_Swapping_Data.java
@@ -0,0 +1,42 @@
+  public void swapNodes(int x, int y) 
+    { 
+        // Nothing to do if x and y are same 
+        if (x == y) return; 
+  
+        // Search for x (keep track of prevX and CurrX) 
+        Node prevX = null, currX = head; 
+        while (currX != null && currX.data != x) 
+        { 
+            prevX = currX; 
+            currX = currX.next; 
+        } 
+  
+        // Search for y (keep track of prevY and currY) 
+        Node prevY = null, currY = head; 
+        while (currY != null && currY.data != y) 
+        { 
+            prevY = currY; 
+            currY = currY.next; 
+        } 
+  
+        // If either x or y is not present, nothing to do 
+        if (currX == null || currY == null) 
+            return; 
+  
+        // If x is not head of linked list 
+        if (prevX != null) 
+            prevX.next = currY; 
+        else //make y the new head 
+            head = currY; 
+  
+        // If y is not head of linked list 
+        if (prevY != null) 
+            prevY.next = currX; 
+        else // make x the new head 
+            head = currX; 
+  
+        // Swap next pointers 
+        Node temp = currX.next; 
+        currX.next = currY.next; 
+        currY.next = temp; 
+    } 
