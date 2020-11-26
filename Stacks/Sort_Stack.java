6fb9098301eee180068785798d2fc55a5720a301
Raj Rawat <rawatraj995@gmail.com>
2020-11-26 19:33:08 +0530

    Update Sort_Stack.java

--- Stacks/Sort_Stack.java
+++ Stacks/Sort_Stack.java
@@ -1,3 +1,16 @@
+//Brute-Force
+     static void calculateSpan(int price[], int n, int S[]) 
+    { 
+        S[0] = 1; 
+        for (int i = 1; i < n; i++) { 
+            S[i] = 1; 
+            for (int j = i - 1; (j >= 0) && (price[i] >= price[j]); j--) 
+                S[i]++; 
+        } 
+    } 
+
+// Efficient-Approach
+
 public Stack<Integer> sort(Stack<Integer> s)
 	{
 	    if(s == null || s.isEmpty()) {
