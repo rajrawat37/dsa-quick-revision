0d006e6b1edbf76eb9e028d18b0c056ed151f101
Raj Rawat <rawatraj995@gmail.com>
2021-01-16 23:25:15 +0530

    Update Remove_Outer_parenthesis.java

--- Stacks/Remove_Outer_parenthesis.java
+++ Stacks/Remove_Outer_parenthesis.java
@@ -44,3 +44,30 @@ public String removeOuterParentheses(String S) {
 	}
 	return s.toString();
 }
+
+//Approach-3 (In cpp)
+
+string removeOuterParentheses(string S) {
+    stack<char>st;
+    string ans;
+    for(auto a:S)
+    {
+        if(a=='(')
+        {
+            if(st.size()>0)
+            {
+                ans+='(';
+            }
+            st.push('(');
+        }
+        else
+        {
+            if(st.size()>1)
+            {
+                ans+=')';
+            }
+            st.pop();
+        }
+    }
+    return ans;
+}
