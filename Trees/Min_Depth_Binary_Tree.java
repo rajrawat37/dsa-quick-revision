9ad0f009c7885296e6a7193583d1938cf480b83a
Raj Rawat <rawatraj995@gmail.com>
2021-01-30 12:00:09 +0530

    Create Min_Depth_Binary_Tree.java

---
+++ Trees/Min_Depth_Binary_Tree.java
@@ -0,0 +1,26 @@
+	int minDepth(Node root)
+	{
+	   if(root==null)
+	     return 0;
+	   
+	  Queue<Node> q= new LinkedList<>();
+	  q.add(root);
+	  int count=1;
+	  while(!q.isEmpty()){
+	        
+	        int len=q.size();
+	        for(int i=0;i<len;i++){ 
+	            Node temp = q.poll();
+	           
+	           if(temp.left==null && temp.right==null)
+	              return count;
+	            
+	           if(temp.left!=null)
+	              q.add(temp.left);
+	            if(temp.right!=null)
+	               q.add(temp.right);
+	        }
+	        count++;
+	  }
+	  return count;
+	}
