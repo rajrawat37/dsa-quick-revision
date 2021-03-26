dccc0da55e85b55290cb72208e918d3e49fe09bc
Raj Rawat <rawatraj995@gmail.com>
2021-03-26 08:06:19 +0530

    Create Detect_Loop.java

---
+++ LinkedList/Detect_Loop.java
@@ -0,0 +1,27 @@
+// Approach-1
+
+public static boolean detectLoop(Node h){
+      HashSet<Node> s = new HashSet<Node>(); 
+        while (h != null) { 
+            if (s.contains(h)) 
+                return true; 
+            s.add(h); 
+            h = h.next; 
+        } 
+        return false; 
+}
+
+//Approach-2 (Floyd's Algorithm)
+
+ public static boolean detectLoop(Node head){
+      Node slow = head, fast = head; 
+        int flag = 0; 
+        while (slow != null && fast!=null && fast.next != null) { 
+            slow = slow.next; 
+            fast = fast.next.next; 
+            if (slow == fast) { 
+                return true;
+            } 
+        } 
+     return false;
+}
