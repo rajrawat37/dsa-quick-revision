97332718080d662db457e2a06fcffd0197978a5f
Raj Rawat <rawatraj995@gmail.com>
2021-05-22 09:49:31 +0530

    Create Remove_Duplicates_Sorted_LL.java

---
+++ LinkedList/Remove_Duplicates_Sorted_LL.java
@@ -0,0 +1,12 @@
+ Node removeDuplicates(Node head)
+    {
+        Node curr=head;
+        while(curr.next!=null){
+             if(curr.data==curr.next.data)
+               curr.next=curr.next.next;
+             else
+               curr=curr.next;
+        }
+        return head;
+	 
+    }
