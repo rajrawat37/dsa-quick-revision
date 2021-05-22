32c57f24a49081502cae703cfb36143dce67db34
Raj Rawat <rawatraj995@gmail.com>
2021-05-22 11:11:49 +0530

    Update Pairwise_Swap_Elements.java

--- LinkedList/Pairwise_Swap_Elements.java
+++ LinkedList/Pairwise_Swap_Elements.java
@@ -9,3 +9,26 @@
         }
         return  head;
     }
+
+
+//Wihtout swapping data by interchanging links
+
+
+ public Node pairwiseSwap(Node head)
+    {
+        Node curr=new Node(0);
+        curr.next=head;
+        Node temp=curr;
+        while(curr.next!=null && curr.next.next!=null){
+            Node first=curr.next;
+            Node second=curr.next.next;
+            first.next=second.next;
+            curr.next=second;
+            second.next=first;
+            curr=curr.next.next;
+        }
+        return temp.next;
+    }
+
+//T.C = O(n)
+//S.C = O(1)
