eac81a7cd7ebc623e905b40bac475e2d544ee8ef
Raj Rawat <rawatraj995@gmail.com>
2021-05-23 10:40:36 +0530

    Update Intersection_Point_In_Y_Shapes_Linked_List.java

--- LinkedList/Intersection_Point_In_Y_Shapes_Linked_List.java
+++ LinkedList/Intersection_Point_In_Y_Shapes_Linked_List.java
@@ -32,3 +32,21 @@
 	    }
 	    return num;
 	}
+
+
+
+//Optimal Approach
+
+	int intersectPoint(Node head1, Node head2)
+	{
+	    if(head1==null || head2==null) return -1;
+	    
+	    Node a=head1;
+	    Node b=head2;
+         while(a!=b){
+             a=a==null?head2:a.next;
+             b=b==null?head1:b.next;
+             
+         }
+         return (a==null)?-1:a.data;
+	}
