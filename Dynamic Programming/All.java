213dd10a695564c4e843f05d6359a11297561c24
Raj Kumar Rawat <rawatraj995@gmail.com>
2025-07-21 08:40:46 +0530

    upd: Revision dp

---
+++ Dynamic Programming/All.java
@@ -0,0 +1,10 @@
+1) Longest Palindromic substring => dp[i][i+len-1] : True if substring(i,i+len-1) is palindrome.
+2) Edit Distance => dp[i][j] : minimum steps to make s1(i,n) = s2(j,n)  // Two strings, find min steps to make them equal using operations : insert,replace or delete
+3) Wildcard Matching => 
+4) Interleaving String => 
+5) Palindrome Partitioning 2 =>
+6) Tallest Billboard
+7) Shortest Common Supersequence
+8) Stone Game 3 => 
+9) Reducing Dishes =>
+10) Number of Ways to Paint N × 3 Grid => 
